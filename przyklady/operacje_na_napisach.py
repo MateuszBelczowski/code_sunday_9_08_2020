30d7e6e77b2ff9779dfa8b75c4761eb3b003c904
Mateusz Bełczowski <mateusz.belczowski@gmail.com>
2020-08-09 12:03:14 +0200

    Operacje na napisach

---
+++ przyklady/operacje_na_napisach.py
@@ -0,0 +1,13 @@
+tekst = "0123456789"
+
+print(len(tekst))
+
+print(tekst[1])
+
+print(tekst[0:5])
+print(tekst[::-1]) # tekst odwrócony
+
+print(tekst[-1])
+liczba_znakow = len(tekst)
+
+print(f"Liczba znaków w tekście to {len(tekst)}")
