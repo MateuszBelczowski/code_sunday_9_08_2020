0723ee1c4e065e7bd652f1b2c68b858043d4a005
Mateusz Bełczowski <mateusz.belczowski@gmail.com>
2020-08-09 11:21:36 +0200

    Prosty kalkulator

---
+++ przyklady/kalkulator.py
@@ -0,0 +1,23 @@
+wynik_dodawania = 10 + 12
+print(wynik_dodawania)
+
+wynik_odejmowania = 12 - 100
+print(wynik_odejmowania)
+
+wynik_mnozenie = 12 * 10
+print(wynik_mnozenie)
+
+wynik_dzielenia1 = 15 / 2
+print(wynik_dzielenia1)
+
+wynik_dzielenia2 = 15 // 2
+print(wynik_dzielenia2)
+
+wynik_potegowania = 2**3
+print(wynik_potegowania)
+
+modulo = 5 % 3
+print(modulo)
+
+bardziej_skomplikowane_obliczenie = 12 * 13 + 18 / 3 + 5*20*3
+print(bardziej_skomplikowane_obliczenie)
