1a4515f12b1ac69b03f3cecf7806fb4a7aa69207
Mateusz Bełczowski <mateusz.belczowski@gmail.com>
2020-08-09 11:09:32 +0200

    Typy zmiennych

---
+++ przyklady/typy_zmiennych.py
@@ -0,0 +1,12 @@
+wynik_z_egzaminu = 95 # liczba
+
+tekst = "tekst1" # napis
+
+tekst_liczbowy = "95" # napis
+
+wspolczynnik = 0.56 # float
+
+czy_wygralismy = True # zmienna logiczna
+czy_wygralismy = False # zmienna logiczna
+
+# true nam nie zadziała, bo Python patrzy na wielkość liter
