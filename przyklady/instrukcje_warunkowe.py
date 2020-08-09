3d9a5d2cfadfe318a51dcc2a19c418abac06afba
Mateusz Bełczowski <mateusz.belczowski@gmail.com>
2020-08-09 12:43:48 +0200

    Instrukcje warunkowe

---
+++ przyklady/instrukcje_warunkowe.py
@@ -0,0 +1,13 @@
+warunek = "" # zachowa się jak False
+warunek2 = "niepusty napis" # zachowa się jak True
+if warunek:
+    print("tekst1")
+    print("tekst2")
+elif warunek2:
+    print("warunek2 jest spełniony")
+else:
+    print("warunek nie jest spełniony")
+print("zawsze jestem wyświetlany")
+
+# truthy
+# falsey/falsy
