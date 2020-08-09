4397744c975feaad096b25b12ba5e83283cd62cf
Mateusz Bełczowski <mateusz.belczowski@gmail.com>
2020-08-09 10:50:30 +0200

    Wyświetlanie tekstu piosenki

---
+++ przyklady/rodzaje_cudzyslowow.py
@@ -0,0 +1,6 @@
+print('hello world')
+print("hello world")
+print('''hello world
+kolejna linijka
+jeszcze jedna
+i kolejna''')
---
+++ quiz.py
@@ -0,0 +1,56 @@
+print("""To Twoja flaga, nasz młody przyjacielu
+Nie musisz kochać jej barw, o nie
+To Twoja armia i życie w ciągłym biegu
+Nigdy nie będziesz już sam
+Możesz wreszcie zachłysnąć się powietrzem
+I unieść do góry jak ptak, he-hej
+Możesz wreszcie zabłądzić w wielkim mieście
+Urodziłeś się, by służyć nam
+
+Właśnie nadszedł ten czas
+Whoah, to jest właśnie ten czas
+
+Ref.:
+Jesteś sterem, białym żołnierzem
+Nosisz spodnie, więc walcz
+Jesteś żaglem, szalonym wiatrem
+Twoja siła to skarb
+
+Bóg jest z nami, jego prawda
+Jak tarcza Cię ocali
+Czekałeś na ten dzień tyle lat
+Ruszaj z nami, z wątłymi marzeniami
+Z ufnością, którą jeszcze masz
+
+Właśnie nadszedł ten czas
+Whoah, to jest właśnie ten czas
+
+Jesteś sterem, białym żołnierzem
+Nosisz spodnie, więc walcz
+Jesteś żaglem, szalonym wiatrem
+Twoja siła to skarb
+
+Jesteś sterem, białym żołnierzem
+Nosisz spodnie, więc walcz
+Jesteś żaglem, szalonym wiatrem
+Twoja siła to skarb
+
+Jesteś sterem, białym żołnierzem
+Nosisz spodnie, więc walcz
+Jesteś żaglem, szalonym wiatrem
+Twoja siła to skarb
+
+Jesteś sterem, białym żołnierzem
+Nosisz spodnie, więc walcz
+Jesteś żaglem, szalonym wiatrem
+Twoja siła to skarb
+
+Jesteś sterem, białym żołnierzem
+Nosisz spodnie, więc walcz
+Jesteś żaglem, szalonym wiatrem
+Twoja siła to skarb
+
+Jesteś sterem, białym żołnierzem
+Nosisz spodnie, więc walcz
+Jesteś żaglem, szalonym wiatrem
+Twoja siła to skarb""")
